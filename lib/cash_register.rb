9fd16e3901872253bb2c3a26e4ba3b7c6c102eb2
Jordan Kline <jordan.kline@gmail.com>
2018-01-09 17:43:42 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,9 @@ attr_accessor :total, :discount, :last_transaction, :items
   def add_item(item, price, qty=1)
     @last_transaction = price * qty
     self.total += @last_transaction
-    self.items << item
+    qty.times do
+      self.items << item
+    end
     binding.pry
   end
 
