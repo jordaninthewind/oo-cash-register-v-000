1995f7c144f7d18dde0d7f1213644a840d8d0687
Jordan Kline <jordan.kline@gmail.com>
2018-01-09 17:27:16 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -36,7 +36,7 @@ attr_accessor :total, :discount, :last_transaction
   end
 
   def void_last_transaction
-    self.total -= @last_transaction
+    # self.total -= @last_transaction
     # self.items.pop
   end
 
