a553e4037afcc8b84a7753ca357d8598b24aa905
Jordan Kline <jordan.kline@gmail.com>
2018-01-09 17:08:44 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,7 @@ attr_accessor :total, :discount
     # binding.pry
     if @discount != nil
       self.total -= self.total * (@discount * 0.01)
-      @total.to_i!
+      self.total = self.total.to_i
       return "After the discount, the total comes to $#{self.total}."
     else
       return "There is no discount to apply."
